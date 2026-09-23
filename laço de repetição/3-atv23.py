21422d27a07d9045ff8c7127eacbd1d56a9acce2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:57:37 -0300

    laco de repeticao for

---
+++ laço de repetição/3-atv23.py
@@ -0,0 +1,11 @@
+import os
+import time
+os.system ("cls")
+
+numero = int(input ("digite um numero:" ))
+
+for i in range (numero,0,-1):
+    print (i)
+    time.sleep(1)
+
+print ("FIM DA CONTAGEM ")
